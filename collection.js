e69e0089b1a3bba6d0c6eeeb5ec1a0d1c807e008
Gokul-E <143737929+Gokul-E@users.noreply.github.com>
2024-09-23 14:25:41 +0530

    Update 1 collection.js

--- collection.js
+++ collection.js
@@ -3,15 +3,16 @@ var productcontainer = document.getElementById("product");
 var search = document.getElementById("search");
 var productlist = productcontainer.querySelectorAll("div");
 
-search.addEventListener("keyup", function(event) {
-    var enteredvalue = event.target.value.toUpperCase();
-    for (var count = 0; count < productlist.length; count++) 
-        {
-        var productname = productlist[count].querySelector("p").textContent.toUpperCase();
-        if (productname.indexOf(enteredvalue) < 0) {
-            productlist[count].style.display = "none";
-        } else {
-            productlist[count].style.display = "block";
-        }
+search.addEventListener("keyup", function (event) {
+  var enteredvalue = event.target.value.toUpperCase();
+  for (var count = 0; count < productlist.length; count++) {
+    var productname = productlist[count]
+      .querySelector("p")
+      .textContent.toUpperCase();
+    if (!productname.includes(enteredvalue)) {
+      productlist[count].style.display = "none";
+    } else {
+      productlist[count].style.display = "block";
     }
+  }
 });
